70db1ec307e13644f61cc4a3af50f13a99772880
tugasSBD <ian25yola@gmail.com>
2023-03-16 03:15:18 +0700

    CREATE : More Button on detail inbox

---
+++ src/components/MoreComponent.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import more from "../images/svg-icon/more.svg";
+
+const MoreComponent = () => {
+  const [isClicked, setIsClicked] = useState(false);
+  const clickHandler = () => {
+    setIsClicked(!isClicked);
+  };
+  return (
+    <MoreStyled>
+      <img src={more} alt='' onClick={clickHandler} />
+      {isClicked && (
+        <div className='more-information text-16'>
+          <div className='information-item edit text-color-primary'>Edit</div>
+          <div className='information-item delete'>Delete</div>
+        </div>
+      )}
+    </MoreStyled>
+  );
+};
+
+const MoreStyled = styled.div`
+  cursor: pointer;
+  position: relative;
+
+  .more-information {
+    position: absolute;
+    background: #ffffff;
+    width: 126px;
+    border: 1px solid #bdbdbd;
+    border-radius: 8px;
+    .information-item {
+      padding: 15px 15px;
+      &.delete {
+        color: #eb5757;
+        border-top: 1px solid #bdbdbd;
+      }
+    }
+  }
+`;
+
+export default MoreComponent;
